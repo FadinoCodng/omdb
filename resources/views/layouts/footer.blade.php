3e05165acd9820086ecd74dd67174f83bb3c8eba
pakdeeganteng <abdulhadipyk8@gamail.com>
2026-04-09 10:01:23 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    Copyright &copy; 2026 
+    <div class="bullet"></div> 
+    Design By <a href="">Fadino</a>
+  </div>
+  <div class="footer-right">
+    
+  </div>
+</footer>
